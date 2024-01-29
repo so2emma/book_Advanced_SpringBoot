59a353d826872ae478e70771acd0559099a301d8
so2emma <osoemmanuel1969@gmail.com>
2024-01-29 11:45:54 +0100

    The book model

---
+++ src/main/java/com/example/books/model/Book.java
@@ -0,0 +1,33 @@
+package com.example.books.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Year;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "books")
+public class Book {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+    private String genre;
+    private String isbn;
+    private Year publicationYear;
+
+
+
+
+
+}
