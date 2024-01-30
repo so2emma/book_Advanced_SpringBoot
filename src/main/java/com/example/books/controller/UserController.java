b7e6306ddc11d793f404f7e2c448745368378dd1
so2emma <osoemmanuel1969@gmail.com>
2024-01-30 19:41:20 +0100

    complete user scaffolding

---
+++ src/main/java/com/example/books/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.books.controller;
+
+import com.example.books.model.User;
+import com.example.books.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @Autowired
+    public UserServiceImpl userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Optional<User>> getUserByName(@PathVariable String name) {
+        return userService.getUserByName(name);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+}
---
+++ src/main/java/com/example/books/model/User.java
@@ -0,0 +1,16 @@
+package com.example.books.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String phone;
+    private String email;
+}
