8ef5557bd458532ab7d6a6bac873d50c421a0e7c
so2emma <osoemmanuel1969@gmail.com>
2024-01-30 19:40:51 +0100

    user service logic implementation.

---
+++ src/main/java/com/example/books/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.books.service;
+
+
+import com.example.books.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface UserService {
+    ResponseEntity<List<User>> getAllUsers();
+    ResponseEntity<Optional<User>> getUserById(Long id);
+    ResponseEntity<Optional<User>> getUserByName(String name);
+    ResponseEntity<User> addUser(User user);
+    ResponseEntity<User> updateUser(Long id, User user);
+
+//    ResponseEntity<Void> deleteUser(Long id);
+}
---
+++ src/main/java/com/example/books/service/impl/UserServiceImpl.java
@@ -0,0 +1,75 @@
+package com.example.books.service.impl;
+
+import com.example.books.model.User;
+import com.example.books.repositories.UserRepositories;
+import com.example.books.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    public UserRepositories userRepositories;
+    @Override
+    public ResponseEntity<List<User>> getAllUsers() {
+        try{
+            return new ResponseEntity<>(userRepositories.findAll(), HttpStatus.OK);
+        }catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    public ResponseEntity<Optional<User>> getUserById(Long id) {
+        try{
+            return new ResponseEntity<>(userRepositories.findById(id), HttpStatus.OK);
+        }catch(Exception e) {
+            e.printStackTrace(System.out);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    public ResponseEntity<Optional<User>> getUserByName(String name) {
+        try{
+            return new ResponseEntity<>(userRepositories.findByName(name), HttpStatus.OK);
+        }catch(Exception e) {
+            e.printStackTrace(System.out);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    public ResponseEntity<User> addUser(User user) {
+        try{
+            return new ResponseEntity<>(userRepositories.save(user), HttpStatus.CREATED);
+        }catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    public ResponseEntity<User> updateUser(Long id, User updatedUser) {
+        Optional<User> optionalUser = userRepositories.findById(id);
+        if(optionalUser.isEmpty())
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        User user = optionalUser.get();
+        user.setName(updatedUser.getName());
+        user.setEmail(updatedUser.getEmail());
+        user.setPhone(updatedUser.getPhone());
+
+        return new ResponseEntity<>(userRepositories.save(user), HttpStatus.OK);
+    }
+}
